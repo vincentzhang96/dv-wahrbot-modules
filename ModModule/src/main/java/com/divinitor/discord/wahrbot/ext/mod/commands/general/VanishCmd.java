4ad9bcf9b842ed7b9e2f0eba58deee6df69417fd
Vincent Zhang <vzhang@purdue.edu>
2017-12-09 14:45:17 -0500

    Vanish basis

---
+++ ModModule/src/main/java/com/divinitor/discord/wahrbot/ext/mod/commands/general/VanishCmd.java
@@ -0,0 +1,64 @@
+package com.divinitor.discord.wahrbot.ext.mod.commands.general;
+
+import com.divinitor.discord.wahrbot.core.command.*;
+import com.divinitor.discord.wahrbot.core.i18n.Localizer;
+import com.google.inject.Inject;
+import net.dv8tion.jda.core.EmbedBuilder;
+
+import java.awt.*;
+import java.util.Locale;
+import java.util.Map;
+import java.util.StringJoiner;
+
+import static com.divinitor.discord.wahrbot.core.command.CommandConstraints.hasAny;
+import static net.dv8tion.jda.core.Permission.MESSAGE_MANAGE;
+
+public class VanishCmd implements Command {
+
+    public static final String KEY = "com.divinitor.discord.wahrbot.ext.mod.commands.general.vanish";
+    private final Localizer loc;
+
+    @Inject
+    public VanishCmd(Localizer loc) {
+        this.loc = loc;
+    }
+
+    @Override
+    public CommandResult invoke(CommandContext context) {
+        CommandLine line = context.getCommandLine();
+        Map<String, Object> nlcp = context.getNamedLocalizationContextParams();
+        Locale l = context.getLocale();
+        if (!line.hasNext()) {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.setTitle(this.loc.localizeToLocale(this.key("error.title"), l, nlcp));
+            builder.setDescription(this.loc.localizeToLocale(this.key("error.no_args"), l, nlcp));
+            builder.setColor(Color.RED);
+            context.getFeedbackChannel().sendMessage(builder.build())
+                .queue();
+            return CommandResult.rejected();
+        }
+
+
+        return null;
+    }
+
+    @Override
+    public CommandConstraint<CommandContext> getUserPermissionConstraints() {
+        return hasAny(MESSAGE_MANAGE);
+    }
+
+    @Override
+    public CommandConstraint<CommandContext> getBotPermissionConstraints() {
+        return hasAny(MESSAGE_MANAGE);
+    }
+
+    public String key(String... children) {
+        StringJoiner joiner = new StringJoiner(".");
+        joiner.add(KEY);
+        for (String child : children) {
+            joiner.add(child);
+        }
+
+        return joiner.toString();
+    }
+}
