a0623897e5e8f001b5b4a84a629eead9b3fc28d2
Vincent Zhang <vahr@divinitor.com>
2020-09-18 19:58:59 -0700

    Stab HP stuff

--- FunModule/src/main/java/com/divinitor/discord/wahrbot/ext/fun/command/StabCommand.java
+++ FunModule/src/main/java/com/divinitor/discord/wahrbot/ext/fun/command/StabCommand.java
@@ -5,9 +5,11 @@ import com.divinitor.discord.wahrbot.core.command.*;
 import com.divinitor.discord.wahrbot.core.i18n.Localizer;
 import com.divinitor.discord.wahrbot.core.i18n.LocalizerBundle;
 import com.divinitor.discord.wahrbot.core.i18n.ResourceBundleBundle;
+import com.divinitor.discord.wahrbot.core.store.MemberStore;
 import com.divinitor.discord.wahrbot.core.util.RateLimiter;
 import com.divinitor.discord.wahrbot.core.util.discord.MemberResolution;
 import com.divinitor.discord.wahrbot.ext.fun.FunModule;
+import com.google.common.base.Strings;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
@@ -18,6 +20,8 @@ import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.SelfUser;
 import net.dv8tion.jda.core.entities.User;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -26,6 +30,12 @@ public class StabCommand extends AbstractKeyedCommand {
     public static final String COMMAND_ID = "stab";
     public static final String KEY = FunModule.MODULE_KEY + ".commands." + COMMAND_ID;
     public static final String STORAGE_KEY_ALT_RATE = "ext.fun.stab.altrate";
+    public static final String STORAGE_KEY_MEMBER_HP = "ext.fun.stab.member.hp";
+    public static final String STORAGE_KEY_MEMBER_LAST_REGEN_TIME = "ext.fun.stab.member.regen.last";
+    public static final String STORAGE_KEY_MEMBER_LAST_KILLED_TIME = "ext.fun.stab.member.killed.last";
+    public static final int DEFAULT_HP = 20;
+    public static final int REGEN_RATE_PER_MINUTE = 2;
+    public static final int RESPAWN_TIME_MINUTES = 5;
 
     private final WahrBot bot;
 
@@ -40,13 +50,13 @@ public class StabCommand extends AbstractKeyedCommand {
     public StabCommand(WahrBot bot) {
         this.bot = bot;
         this.rateLimiters = CacheBuilder.newBuilder()
-            .expireAfterAccess(5, TimeUnit.MINUTES)
-            .build(new CacheLoader<String, RateLimiter>() {
-                @Override
-                public RateLimiter load(String key) throws Exception {
-                    return new RateLimiter(key, TimeUnit.SECONDS.toMillis(15), 3);
-                }
-            });
+                .expireAfterAccess(5, TimeUnit.MINUTES)
+                .build(new CacheLoader<String, RateLimiter>() {
+                    @Override
+                    public RateLimiter load(String key) throws Exception {
+                        return new RateLimiter(key, TimeUnit.SECONDS.toMillis(15), 3);
+                    }
+                });
         this.random = new Random();
     }
 
@@ -86,14 +96,13 @@ public class StabCommand extends AbstractKeyedCommand {
             return CommandResult.error();
         }
 
-        String msg = this.stab(context, invokerMember, targetMember);
+        this.stab(context, invokerMember, targetMember);
 
-        context.getFeedbackChannel().sendMessage(msg).queue();
 
         return CommandResult.ok();
     }
 
-    private String stab(CommandContext context, Member source, Member target) {
+    private void stab(CommandContext context, Member source, Member target) {
         Locale locale = context.getLocale();
         Map<String, Object> nlcp = context.getNamedLocalizationContextParams();
         Localizer localizer = context.getLocalizer();
@@ -105,29 +114,50 @@ public class StabCommand extends AbstractKeyedCommand {
 
         if (target.getUser().getIdLong() == botUser.getIdLong()) {
             // Stab self
-            return localizer.localizeToLocale(
-                key("resp.self"),
-                locale,
-                selfName,
-                nlcp);
-        } else if (this.isSudo(target)) {
-            if (this.isSudo(source)) {
-                // Admin stabbing self -> bot stabs itself
-                return localizer.localizeToLocale(
+            String msg = localizer.localizeToLocale(
                     key("resp.self"),
                     locale,
                     selfName,
                     nlcp);
+            context.getFeedbackChannel().sendMessage(msg).queue();
+            return;
+        } else if (this.isSudo(target)) {
+            if (this.isSudo(source)) {
+                // Admin stabbing self -> bot stabs itself
+                String msg = localizer.localizeToLocale(
+                        key("resp.self"),
+                        locale,
+                        selfName,
+                        nlcp);
+
+                context.getFeedbackChannel().sendMessage(msg).queue();
+                return;
             } else {
                 // Person stabbing admin -> bot stabs source
-                return localizer.localizeToLocale(
-                    key("resp.third_person"),
-                    locale,
-                    selfName,
-                    sourceName,
-                    nlcp);
+                String msg = localizer.localizeToLocale(
+                        key("resp.third_person"),
+                        locale,
+                        selfName,
+                        sourceName,
+                        nlcp);
+
+                context.getFeedbackChannel().sendMessage(msg).queue();
+                return;
             }
         } else {
+            this.simTick(source, target);
+
+            if (this.getMemberHp(source) <= 0 && !this.isSudo(source)) {
+                // Dead people can't stab
+                String msg = localizer.localizeToLocale(
+                        key("member.are_dead"),
+                        locale,
+                        nlcp);
+
+                context.getFeedbackChannel().sendMessage(msg).queue();
+                return;
+            }
+
             // Regular or alt stab
             float alternateStabRate = getAlternateStabRate(context, source, target);
             float roll = this.random.nextFloat();
@@ -136,12 +166,15 @@ public class StabCommand extends AbstractKeyedCommand {
                 key = getAlternateStabKey(context);
             }
 
-            return localizer.localizeToLocale(
-                key,
-                locale,
-                sourceName,
-                targetName,
-                nlcp);
+            String msg = localizer.localizeToLocale(
+                    key,
+                    locale,
+                    sourceName,
+                    targetName,
+                    nlcp);
+            context.getFeedbackChannel().sendMessage(msg).queue();
+
+            applyHpModAmount(key, source, target, context);
         }
     }
 
@@ -153,27 +186,27 @@ public class StabCommand extends AbstractKeyedCommand {
     private float getAlternateStabRate(CommandContext context, Member invoker, Member target) {
         double[] rates = new double[4];
         rates[0] = this.bot.getDynConfigStore()
-            .getDouble(STORAGE_KEY_ALT_RATE, 0.05D);
+                .getDouble(STORAGE_KEY_ALT_RATE, 0.05D);
 
         //  Check if this server has a different rate
         rates[1] = this.bot.getServerStorage().forServer(context.getServer())
-            .getDouble(STORAGE_KEY_ALT_RATE, 0D);
+                .getDouble(STORAGE_KEY_ALT_RATE, 0D);
 
         //  Check if the target user has a different rate
         rates[2] = this.bot.getUserStorage().forUser(target.getUser())
-            .getDouble(STORAGE_KEY_ALT_RATE, 0D);
+                .getDouble(STORAGE_KEY_ALT_RATE, 0D);
 
         //  Check if the invoking user has a different rate
         rates[3] = this.bot.getUserStorage().forUser(invoker.getUser())
-            .getDouble(STORAGE_KEY_ALT_RATE, 0D);
+                .getDouble(STORAGE_KEY_ALT_RATE, 0D);
 
         return (float) Arrays.stream(rates).max().orElse(0);
     }
 
     private String getAlternateStabKey(CommandContext context) {
         List<String> keys = this.localizerBundle.keys(context.getLocale())
-            .filter((k) -> k.startsWith("ext.fun.commands.stab.resp.alt."))
-            .collect(Collectors.toList());
+                .filter((k) -> k.startsWith("ext.fun.commands.stab.resp.alt.") && !k.endsWith(".hp"))
+                .collect(Collectors.toList());
 
         return keys.get(this.random.nextInt(keys.size()));
     }
@@ -190,21 +223,241 @@ public class StabCommand extends AbstractKeyedCommand {
     }
 
     private static String escape(String text) {
-        return text
-            .replace("@everyone", "@ everyone")
-            .replace("@here", "@ here");
+        String sanitized = text
+                .replace("@", "@ ")
+                .replace("{", "\\{")
+                .replace("[", "\\[");
+        return sanitized;
     }
 
     /**
      * Register this command to the given registry and localizer
+     *
      * @param commandRegistry The registry to add this command to
-     * @param localizer The localizer to add this command to
+     * @param localizer       The localizer to add this command to
      */
     public void register(CommandRegistry commandRegistry, Localizer localizer) {
         this.localizerBundle = new ResourceBundleBundle(this.getResourcePath(),
-            this.getClass().getClassLoader());
+                this.getClass().getClassLoader());
         localizer.registerBundle(this.key(),
-            this.localizerBundle);
+                this.localizerBundle);
         commandRegistry.registerCommand(this, this.key());
+    }
+
+    private MemberStore getMemberStore(Member member) {
+        return this.bot.getServerStorage().forServer(member.getGuild()).forMember(member);
+    }
+
+    private int getMemberHp(Member member) {
+        return getMemberStore(member).getInt(STORAGE_KEY_MEMBER_HP, DEFAULT_HP);
+    }
+
+    private void setMemberHp(Member member, int hp) {
+        getMemberStore(member).put(STORAGE_KEY_MEMBER_HP, hp);
+    }
+
+    private void applyRespawn(Member member) {
+        int hp = this.getMemberHp(member);
+        if (hp <= 0) {
+            // Player is dead, check when they died
+            MemberStore store = getMemberStore(member);
+            long lastKilledTimeMillis = store.getLong(STORAGE_KEY_MEMBER_LAST_KILLED_TIME, 0);
+            if (lastKilledTimeMillis == 0) {
+                return;
+            }
+
+            Instant lastKilledTime = Instant.ofEpochMilli(lastKilledTimeMillis);
+            Instant now = Instant.now();
+            long minutes = Duration.between(now, lastKilledTime).abs().toMinutes();
+            if (minutes >= RESPAWN_TIME_MINUTES) {
+                // Revive the player
+                setMemberHp(member, DEFAULT_HP);
+                store.put(STORAGE_KEY_MEMBER_LAST_KILLED_TIME, 0);
+                store.put(STORAGE_KEY_MEMBER_LAST_REGEN_TIME, now.toEpochMilli());
+            }
+        }
+    }
+
+    private void applyRegen(Member member) {
+        int hp = this.getMemberHp(member);
+        if (hp < DEFAULT_HP && hp > 0) {
+            // Player needs regen, check when they last had regen applied
+            MemberStore store = getMemberStore(member);
+            long lastRegenTimeMillis = store.getLong(STORAGE_KEY_MEMBER_LAST_REGEN_TIME, 0);
+            Instant now = Instant.now();
+            if (lastRegenTimeMillis == 0) {
+                lastRegenTimeMillis = now.toEpochMilli();
+            }
+
+            Instant lastRegenTime = Instant.ofEpochMilli(lastRegenTimeMillis);
+            long minutes = Duration.between(now, lastRegenTime).abs().toMinutes();
+            if (minutes > 0) {
+                int hpToRestore = (int) (minutes * REGEN_RATE_PER_MINUTE);
+                this.setMemberHp(member, Math.max(DEFAULT_HP, hp + hpToRestore));
+                store.put(STORAGE_KEY_MEMBER_LAST_REGEN_TIME, now.toEpochMilli());
+            }
+        }
+    }
+
+    private void simTick(Member stabber, Member stabbee) {
+        this.applyRespawn(stabber);
+        this.applyRespawn(stabbee);
+
+        this.applyRegen(stabber);
+        this.applyRegen(stabbee);
+    }
+
+    private void reportHp(Member stabber, int stabberHp, int stabberDelta, Member stabbee, int stabbeeHp, int stabbeeDelta, CommandContext context) {
+        Locale locale = context.getLocale();
+        Map<String, Object> nlcp = context.getNamedLocalizationContextParams();
+        Localizer localizer = context.getLocalizer();
+
+        boolean stabberChanged = stabberDelta != 0;
+        boolean stabbeeChanged = stabbeeDelta != 0;
+        boolean stabberDied = stabberHp <= 0;
+        boolean stabbeeDied = stabbeeHp <= 0;
+
+        String stabberName = escape(stabber.getEffectiveName());
+        String stabbeeName = escape(stabbee.getEffectiveName());
+
+        StringJoiner joiner = new StringJoiner("\n");
+
+        joiner.add(
+                localizer.localizeToLocale(
+                        key(stabberChanged ? "member.hp.change" : "member.hp.no_change"),
+                        locale,
+                        stabberName,
+                        -stabberDelta,
+                        stabberHp,
+                        DEFAULT_HP,
+                        nlcp
+                )
+        );
+        joiner.add(
+                localizer.localizeToLocale(
+                        key(stabbeeChanged ? "member.hp.change" : "member.hp.no_change"),
+                        locale,
+                        stabbeeName,
+                        -stabbeeDelta,
+                        stabbeeHp,
+                        DEFAULT_HP,
+                        nlcp
+                )
+        );
+
+        if (stabberDied && stabberDelta != 0) {
+            joiner.add(
+                    localizer.localizeToLocale(
+                            key("member.died"),
+                            locale,
+                            stabberName,
+                            RESPAWN_TIME_MINUTES,
+                            nlcp
+                    )
+            );
+        }
+        if (stabbeeDied && stabbeeDelta != 0) {
+            joiner.add(
+                    localizer.localizeToLocale(
+                            key("member.died"),
+                            locale,
+                            stabbeeName,
+                            RESPAWN_TIME_MINUTES,
+                            nlcp
+                    )
+            );
+        }
+
+        context.getFeedbackChannel().sendMessage(joiner.toString()).queue();
+    }
+
+    private void onDie(Member member) {
+        MemberStore store = getMemberStore(member);
+        Instant now = Instant.now();
+        if (store.getLong(STORAGE_KEY_MEMBER_LAST_KILLED_TIME, 0) == 0) {
+            store.put(STORAGE_KEY_MEMBER_LAST_KILLED_TIME, now.toEpochMilli());
+            store.put(STORAGE_KEY_MEMBER_LAST_REGEN_TIME, now.toEpochMilli());
+        }
+    }
+
+    private void applyHpModAmount(String key, Member stabber, Member stabbee, CommandContext context) {
+        if (stabber.getUser().getIdLong() == stabbee.getUser().getIdLong()) {
+            return;
+        }
+
+        String hpKey = key + ".hp";
+        String hpParams = this.localizerBundle.get(hpKey, context.getLocale());
+        if (Strings.isNullOrEmpty(hpParams)) {
+            return;
+        }
+
+        String[] split = hpParams.split(";");
+        if (split.length != 2) {
+            return;
+        }
+
+        int stabberHp = getMemberHp(stabber);
+        int stabbeeHp = getMemberHp(stabbee);
+
+        int stabberModHp = stabberHp <= 0 ? 0 : getHpModAmount(split[0], stabber);
+        int stabbeeModHp = stabbeeHp <= 0 ? 0 : getHpModAmount(split[1], stabbee);
+
+        int newStabberHp = Math.min(DEFAULT_HP, stabberHp - stabberModHp);
+        int newStabbeeHp = Math.min(DEFAULT_HP, stabbeeHp - stabbeeModHp);
+
+        this.setMemberHp(stabber, newStabberHp);
+        this.setMemberHp(stabbee, newStabbeeHp);
+
+        this.reportHp(stabber, newStabberHp, stabberModHp, stabbee, newStabbeeHp, stabbeeModHp, context);
+
+        if (newStabberHp <= 0) {
+            this.onDie(stabber);
+        }
+
+        if (newStabbeeHp <= 0) {
+            this.onDie(stabbee);
+        }
+    }
+
+    private int getHpModAmount(String param, Member target) {
+        try {
+            if (param.startsWith("s")) {
+                // Special effect
+                String[] split = param.split(",");
+                if (split.length < 2) {
+                    return 0;
+                }
+
+                int effectId = Integer.parseInt(split[1]);
+
+                switch (effectId) {
+                    case 159: {
+                        // COUP
+                        if (split.length >= 5) {
+                            int hp = this.getMemberHp(target);
+                            int baseDamage = Integer.parseInt(split[2]);
+                            float threshold = Float.parseFloat(split[3]);
+                            float power = Float.parseFloat(split[4]);
+                            int damage = baseDamage;
+
+                            float hpPercent = (float) (hp - baseDamage) / DEFAULT_HP;
+                            if (hpPercent <= threshold && hpPercent >= 0) {
+                                float diff = threshold - hpPercent;
+                                damage += baseDamage * (diff * power * 100);
+                            }
+
+                            return damage;
+                        }
+
+                        break;
+                    }
+                }
+            } else {
+                return Integer.parseInt(param);
+            }
+        } catch (NumberFormatException ignored) {
+        }
+
+        return 0;
     }
 }
